f831e6b5b8274e3acc120970ac4781f04687bca4
HAHAZX <hahazx@example.com>
2025-07-09 22:46:57 +0700

    fix: disable ESLint on Vercel build using next.config.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,14 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-export default [
-  // Ini untuk extend default dari Next.js + TypeScript
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // Tambahkan override custom rule di bawah ini
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-    },
-  },
-];
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+ 
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
+]
+ 
+export default eslintConfig
